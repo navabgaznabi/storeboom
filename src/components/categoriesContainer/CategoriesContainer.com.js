133d26c362c1e448490b802747033ed90884fa6c
navabgaznabi <nav.gaznabi@gmail.com>
2023-01-06 01:54:57 +0530

    Added categories Card

---
+++ src/components/categoriesContainer/CategoriesContainer.com.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./categories-container.scss";
+import CategoryItemComp from "../category/category-item.comp";
+
+const CategoriesContainerCom = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        console.log(category);
+        return <CategoryItemComp key={category.id} category={category} />;
+      })}
+    </div>
+  );}
+
+export default CategoriesContainerCom;
